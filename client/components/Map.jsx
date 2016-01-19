942f7f41706c9473882dfb8588ac2ea4d6cbfbb6
Jaysig <jasontsigmon@gmail.com>
2016-01-18 22:03:32 -0500

    added sidebar

---
+++ client/components/Map.jsx
@@ -0,0 +1,20 @@
+Map = React.createClass({
+  render() {
+    return (
+		  <div>
+		    <h1>This is where the map goes</h1>
+		    <Sidenav />
+		  </div>
+		)
+  }
+})
+
+Sidenav = React.createClass({
+  render() {
+    return (
+      <nav className="sidenav">
+        icons
+      </nav>
+    )
+  }
+})
